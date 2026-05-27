fef0fbf064c34b2ba2ad46ddfa5801e295721a28
Amit Kumar <amitrke@gmail.com>
2026-05-26 21:06:51 -0600

    refactor: Enhance MainActivity to support system window insets

--- android/app/src/main/kotlin/org/roorkee/RkeApp/MainActivity.kt
+++ android/app/src/main/kotlin/org/roorkee/RkeApp/MainActivity.kt
@@ -1,6 +1,11 @@
 package org.roorkee.RkeApp
 
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+	override fun onCreate(savedInstanceState: android.os.Bundle?) {
+		WindowCompat.setDecorFitsSystemWindows(window, false)
+		super.onCreate(savedInstanceState)
+	}
 }
